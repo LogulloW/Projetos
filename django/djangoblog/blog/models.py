b3df6ea7839115b46a68b65ad2a1a8f1e969c953
Logullo <75046915+LogulloW@users.noreply.github.com>
2021-11-22 16:16:10 -0300

    djangodev

--- django/djangoblog/blog/models.py
+++ django/djangoblog/blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.title
